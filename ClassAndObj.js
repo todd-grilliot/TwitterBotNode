03e01804520180c1d45fafb5a6b770e6034b57ee
yankeehotelfoxtrot51 <tbgrilliot@gmail.com>
2021-07-31 18:37:19 -0600

    First launch! I think it's ready!

--- ClassAndObj.js
+++ ClassAndObj.js
@@ -1,23 +1,25 @@
 // text, created_at, id_str, retweet_count, fav_count
 
 class tweet_Class{
-    constructor(text, created_at, id_str, retweet_count, fav_count){
+    constructor(text, created_at, id_str, retweet_count, fav_count, retweet_id_str){
         this.text = text;
         this.created_at = created_at;
         this.id_str = id_str;
         this.retweet_count = retweet_count;
         this.fav_count = fav_count;
+        this.retweet_id_str = retweet_id_str;
     }
 }
 
+// just a regular old array of users
 const users = [
     {name: 'Elon Musk', user_id: 44196397},
     {name: 'Neil deGrasse Tyson', user_id: 19725644},
     {name: 'Deiter F. Uchtdorf', user_id: 1444693544},
     {name: 'NASA', user_id: 24919888},
     {name: 'CNN Breaking News', user_id: 428333},
-    {name: 'Ellen DeGeneres', user_id: 15846407},
-    {name: 'Barrak Obama', user_id: 813286}
+    {name: 'Barrak Obama', user_id: 813286},
+    {name: 'Randall Munroe', user_id: 21146468}
 ]
 
 module.exports.Tweeto = tweet_Class;
--- bot.js
+++ bot.js
@@ -1,27 +1,24 @@
 console.log("starting bot.js bois...");
 
-var Twit = require('twit');
-const prompt = require('prompt-sync')({sigint: true});
-//var string = prompt('give me a string'); ^^^
 var Config = require('./config');
-var KnockJokes = require('./knockjokes');
-//KnockJokes.tellJoke(); ^^^
+var Twit = require('twit');
 var ClassyObj = require('./ClassAndObj');
-var Content = require('./content.json');
-var fs = require('fs');
-const { report } = require('process');
 var T = new Twit(Config); //oauth <<<
 
 // *** Global Vars ***
-var dailyCounter = 0;
 var tmCount = 10;
-//var userTweetArray = []; <<< i don't think this has to be a global var anymore, it's now initiated in buildRetweet()
+var myTweetsArray = [];
 var selectedRetweetUser;
 var previousRetweetUser;
+var tries = 0;
+const trylimit = 100;
 // *** End Global Vars ***
 
+//selectUser() looks at the user Array in ClassyObj and selects a random user from the array, 
+//as long as that's not the same as the previousRetweetUser.
+//he takes that user, remembers them for next time, and plugs them into buildRetweetArray()
 function selectUser(){
-  //console.log(JSON.stringify(ClassyObj.retweetUsers));
+  console.log('selecting user for retweet...');
   selectedRetweetUser = ClassyObj.retweetUsers[Math.floor(Math.random() * ClassyObj.retweetUsers.length)];
   if(selectedRetweetUser === previousRetweetUser){
     console.log('repeat user, recalculating...');
@@ -29,86 +26,110 @@ function selectUser(){
   }else{
     console.log(selectedRetweetUser);
     previousRetweetUser = selectedRetweetUser;
-    buildRetweet(selectedRetweetUser.name, selectedRetweetUser.user_id);
+    buildRetweetArray(selectedRetweetUser.name, selectedRetweetUser.user_id);
   }
 }
 
-function buildRetweet(nameParam, idParam){
+//buildRetweetArray() takes the user from selectUser() and T.gets their recent (tmCount) # of posts. 
+//He builds an array of Tweet Objects with that data, and then hands the array to chooseRetweet()
+function buildRetweetArray(nameParam, idParam){
+  console.log(`building an array of their ${tmCount} most recent tweets...`);
   userTweetArray = [];
   T.get('statuses/user_timeline', 
   {name: nameParam, user_id: idParam, count: tmCount}, 
   function (err, data, response){
     if(err){console.log('error! in the t.get...')}else{
-      console.log('getting user_timeline...')
-      //console.log('data:\n' + JSON.stringify(data,null,2))
       //he builds an object and pushes it to an array.
       for (let i = 0; i < data.length; i++) {
-        var userTweet = new ClassyObj.Tweeto(
-          data[i].text,
-          data[i].created_at,
-          data[i].id_str,
-          data[i].retweet_count,
-          data[i].favorite_count
-        );
+        var userTweet = new ClassyObj.Tweeto(data[i].text, data[i].created_at, data[i].id_str, data[i].retweet_count, data[i].favorite_count); //new tweet obj
         userTweetArray.push(userTweet);
       }
       chooseRetweet(userTweetArray);
     }
   });
 }
-function chooseRetweet(retweetArray){
-  console.log(JSON.stringify(retweetArray));
+
+//chooseRetweet() takes the array from buildRetweetArray() and compares all the tweets to eachother.
+//the one with the most .retweet_count is assigned to strongestTweet.
+//You want to then compare the strongest tweet with the recent tweets that you have made. jeez.
+async function chooseRetweet(retweetArray){
+  console.log('choosing something from the array to retweet...');
   var strongestTweet = retweetArray[0];
+  var myRecentTweetsArray = await myRecentTweets();
+
+  if(retweetArray.length < tmCount/3 ){
+    console.log('the retweet array is too small, probably because we retweeted all of their content already, reselecting...');
+    tries++;
+    if(tries > tryLimit){
+      console.log('too many failed attempts. Shutting down... *err in chooseRetweet()*');
+      return
+    };
+    return selectUser();
+  }
+
+  //comparing the array to find the strongestTweet
   for(let i = 0; i < retweetArray.length; i++){
     if(retweetArray[i].retweet_count > strongestTweet.retweet_count){
       strongestTweet = retweetArray[i];
-      console.log('the strongest tweet is number...' + i);
-      console.log(strongestTweet.retweet_count);
-    }
-    // He needs to compare this list to his own previous tweet list. Have him delete parts of the array if he's already tweeted it and choose again.
-    // after he chooses he needs one more callback to retweet the thing he chose.
-  }
-}
-
-//setInterval(selectUser, 1000*60);
-selectUser();
-
-//he's going to need to get other users, maybe like a set of 5 or so just to start.
-// he's going to need to select one, build an array with it, select the most retweetable tweet, and then tweet that.
-
-/** 
-setInterval(DailyTweet, 1000*60*3);
-function DailyTweet(){
-
-  //define tweet content, post function, log statuses, dailyCounter++
-  var tweet = {status: Content[dailyCounter].text}
-  function post(){
-    T.post('statuses/update', tweet, function(err, data, response){
-    console.log("posting: " + Content[dailyCounter].text);
-    console.log("posting image: " + Content[dailyCounter].imagePath);
-    console.log("timestamp: " + data.created_at);
-    console.log("id_string: " + data.id_str);
-    dailyCounter++
-  })}
-  //if there is an image in the tweet, upload it, then post
-  var b64content;
-  if(Content[dailyCounter].imagePath){
-    //init b64 content with our image
-    b64content = fs.readFileSync(Content[dailyCounter].imagePath, { encoding: 'base64' });
-    //T.post upload fuction with our callback
-    T.post('media/upload', {media_data: b64content}, uploadCallback);
-    function uploadCallback(err, data, response){
-      mediaIdArray = [data.media_id_string];
-      //put the media_ids into the tweet object and give it a value vvv
-      tweet.media_ids = mediaIdArray;
-      post();
     }
   }
-  else { post(); }
+
+  //comparing myRecentTweetsArray to the strongest tweet to check for duplicates
+  for(let i = 0; i < myRecentTweetsArray.length; i++){
+    if(myRecentTweetsArray[i].retweet_id_str === strongestTweet.id_str){
+      console.log('one of your recent tweets is the same one that we chose!!!');
+      console.log("we'll have to chose again...");
+      let strongTweetIndex = retweetArray.indexOf(strongestTweet);
+      retweetArray.splice(strongTweetIndex, 1);
+      return chooseRetweet(retweetArray); // <<< run it again.
+      }
+    }
+    //we made it! let's retweet this son of a gun
+    retweet(strongestTweet);
+  }
+
+//myRecentTweets() T.gets my own user timeline, my last tmCount # of posts. it builds an array (myTweetsArray) with reTweetIdStr
+// and then it gives that array back to chooseRetweet.
+function myRecentTweets(){
+  return new Promise((resolve, reject) => {
+    T.get('statuses/user_timeline', {count: tmCount * 3}, (err, data, response) => {
+      //returns an array of my (tmCount) most recent tweets
+      for (let i = 0; i < data.length; i++) {
+        //if var retweetIdStr is unidentified then we set it to null
+        var retweetIdStr;
+        if (data[i].retweeted_status){retweetIdStr = data[i].retweeted_status.id_str;} else {retweetIdStr = null;}
+        
+        var myTweet = new ClassyObj.Tweeto(data[i].text, data[i].created_at, data[i].id_str, data[i].retweet_count, data[i].favorite_count, retweetIdStr); //new tweet obj
+        myTweetsArray.push(myTweet);
+      }
+      resolve(myTweetsArray);
+    });
+  })
 };
+function retweet(theTweet){
+  console.log('Retweeting!!!');
+  console.log(theTweet);
+  tries = 0;
+  
+  T.post('statuses/retweet', {id: theTweet.id_str}, function(err, data, response){
+    if(err){console.log(err);}
+    else{console.log(theTweet); };   
+    })
+  }
+
+setInterval(selectUser, 1000*60*8);
+//selectUser();
+
+/*
+//uses a tweets id_str to find and return the user.
+function findUserIdFromTweet(tweetId){
+  T.get('statuses/show', {id: tweetId}, function(err, data, response){
+    if(err){console.log(err)}
+    else{
+      console.log(`user id: ${data.user.id}`);
+      return data.user.id;
+    }
+  })
+}
+findUserIdFromTweet('1421540186287218690');
 */
-
-//write some code to get the data that you are looking for.
-
-//keep track of certain tweeters and repost everything that is 2 standard deviations above their average engagement rate.
-// neil degrass tyson, byu, byui, holland 
